7dc5a8f70337b9acd972c00f5b3164872e746c27
Jubril Bello <jbello@uoguelph.ca>
2023-05-22 02:19:10 -0400

    implemented function and added emojis

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const successEmoji = "✅✅✅";
+const failedEmoji = "❌❌❌";
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(successEmoji + " Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(failedEmoji + " Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(1,2);
+assertEqual("main","main");
