dd57f0d2c1d7b66421ac2a93b0ec1f3b04dd07f6
Jubril Bello <jbello@uoguelph.ca>
2023-05-22 05:05:08 -0400

    implemented head.js

---
+++ head.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const successEmoji = "✅✅✅";
+const failedEmoji = "❌❌❌";
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${successEmoji} Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`${failedEmoji} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  if (Array.isArray(arr) && arr.length >= 1) {
+    return (arr[0]);
+  }
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
