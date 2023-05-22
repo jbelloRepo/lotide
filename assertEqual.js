e513d74b02a4242d81140ff3f927d63a575c2622
Jubril Bello <jbello@uoguelph.ca>
2023-05-22 02:28:32 -0400

    implemented string interpolation

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const successEmoji = "✅✅✅";
 const failedEmoji = "❌❌❌";
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(successEmoji + " Assertion Passed: " + actual + " === " + expected);
+    console.log(`${successEmoji} Assertion Passed ${actual} === ${expected}`);
   } else {
-    console.log(failedEmoji + " Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${failedEmoji} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
