ca1d0f9b263ee123007090ad57c6c6e0e9a63913
Jubril Bello <jbello@uoguelph.ca>
2023-05-23 01:57:00 -0400

    Tested function implementation

--- without.js
+++ without.js
@@ -24,64 +24,82 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
+//----- FUNCTION IMPLEMENTATION -----------//
 const without = function (arr, itemRemove) {
   let itemRemoveIndex = [];
-  let isRemoved = false;
+  let isSkipped = false;
   let isPresent = false;
   let newArr = [];
 
+  // Get the index of the itemRemove in the arr
   for (let i = 0; i < itemRemove.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      console.log(i, j);
+      // console.log(i, j);
       if (itemRemove[i] === arr[j]) {
         // console.log('equal');
         itemRemoveIndex.push(j);
       }
     }
   }
-  console.log(itemRemoveIndex);
+  // console.log(`The index of items to remove is: ${itemRemoveIndex}`);
 
+  // Iterate over arr skipping indexes in itemRemove
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < itemRemoveIndex.length; j++) {
-      console.log(i, j);
+      // console.log(i, j);
       if (i === itemRemoveIndex[j]) {
-        console.log("True");
-        console.log(`i = ${i}`);
-        isRemoved = true; // false by default
-        // console.log(i, j); [ 0, 3, 4, 1 ] i = 0, 3, 4,1
-        // console.log(i, itemRemoveIndex[j]); [ 0, 4, 5, 1 ]
+        // console.log("skip this");
+        // console.log(`i = ${i}`);
+        isSkipped = true; // false by default
         break;
       } else {
-        isRemoved = false;
-      }
-      // console.log(i, j);
-      // console.log(i, itemRemoveIndex[j]);
-      if (j === itemRemoveIndex.length - 1 && isRemoved === false) {
-        if (newArr.length >= 1) {
-          for (let j = 0; i < newArr.length; j++) {
-            if (arr[i] === newArr[j]) {
-              console.log("The item already exists in the new array");
-              isPresent = true;
-            }
-          }
-          if (isPresent === false) {
-            console.log("adding i....");
-            newArr.push(arr[i]);
-          }
-        } else {
-          console.log("adding i....");
-          newArr.push(arr[i]);
-          console.log(newArr.length);
+        if (j === itemRemoveIndex.length - 1) {
+          isSkipped = false; //false when iteration over itemRemoveIndex is complete
         }
-        // isAdded = false;
-        // break;
       }
-
-      //   break;
     }
+
+    // check if the [i] index is skipped
+    if (isSkipped === false) {
+      // if not not skipped, check if the item at index [i] exist in the new array
+      if (newArr.length >= 1) {
+        // console.log("Array has more than one element");
+        for (let k = 0; k < newArr.length; k++) {
+          if (arr[i] === newArr[k]) {
+            // console.log("The item already exists in the new array");
+            isPresent = true;
+          }
+        }
+        // add it if its not
+        if (isPresent === false) {
+          // console.log("adding i.... The element is not present");
+          newArr.push(arr[i]);
+        }
+      } else { // Add the first element
+        // console.log("adding i....");
+        newArr.push(arr[i]);
+        // console.log(newArr.length);
+      }
+      // isAdded = false;
+      // break;
+    }
+
+    //   break;
   }
-  console.log(newArr);
+  // console.log("\nThe new array is: " + [newArr]);
   return newArr;
 };
+//----- FUNCTION IMPLEMENTATION END -----------//
 
+
+// TESTS FOR WITHOUT IMP
 without([1, 2, 3, 3, 1, 1], [1, 2]);
+without([1, 2, 3, 3, 1, 1, 4, 7, 5, 5, 7], [1, 2, 3, 4]);
+without(["1", "2", "3"], [1, 2, "3"]);
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
