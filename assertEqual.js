39e1b68e84cb5150b45c6e05f5aad140b469654f
Jubril Bello <jbello@uoguelph.ca>
2023-06-11 21:37:33 -0400

    add test folder and update head.js & assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -9,8 +9,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual(1,2);
-assertEqual("main","main");
+
+
+module.exports = assertEqual;
