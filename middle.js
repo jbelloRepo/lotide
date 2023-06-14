a276b9631d64b995011d58b1710231a0372a7d05
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 18:09:07 -0400

    refactor function to remove dead code

--- middle.js
+++ middle.js
@@ -1,4 +1,4 @@
-//----------- FUNCTION IMPLEMENTATION --------------//
+// FUNCTION IMPLEMENTATION
 const middle = function(arr) {
   let middleIndex;
   let newArr;
@@ -13,13 +13,9 @@ const middle = function(arr) {
       middleIndex = Math.ceil(arr.length / 2) - 1;
       newArr = arr.slice(middleIndex, middleIndex + 1);
     }
-    // console.log(middleIndex);
-    // console.log("===middle=== " + newArr);
     return newArr;
   }
 };
-//----------- FUNCTION IMPLEMENTATION END --------------//
-
 
 module.exports = middle;
 
