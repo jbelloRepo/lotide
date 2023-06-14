6a770fb9398c231cb4918c9aed5b6b2b55b42b8a
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 19:53:39 -0400

    Refactor function to remove dead code

--- tail.js
+++ tail.js
@@ -1,16 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const tail = function(arr) {
   if (arr.length === 1) {
-    console.log("The array has no tail");
     return [];
   } else if (arr.length > 1) {
-    // console.log(arr.slice(1));
     const myArr = arr.slice(1);
     return myArr;
   } else {
     return [];
   }
 };
-
 
 module.exports = tail;
