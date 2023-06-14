77e2115f1b66eb79443fbbf7c44d6cb9f4a27577
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 18:24:24 -0400

    refactor function to remove dead code

--- map.js
+++ map.js
@@ -1,34 +1,12 @@
+/**
+ * Description: The map function will take in two arguments:
+ * 1. An array to map
+ * 2. A callback function
+ * The map function will return a new array based on the
+ * results of the callback function.
+ */
+
 // FUNCTION IMPLEMENTATION
-const successEmoji = "✅✅✅";
-const failedEmoji = "❌❌❌";
-const assertArraysEqual = function(arr1, arr2) {
-  const isEqual = eqArrays(arr1, arr2);
-
-  if (isEqual) {
-    console.log(`${successEmoji} Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`${failedEmoji} Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-// Function implementation
-// takes in array and callback function
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -37,7 +15,4 @@ const map = function(array, callback) {
   return results;
 };
 
-const results1 = map(words, (word) => word[0]);
-console.log(results1);
-
-assertArraysEqual([ 'g', 'c', 't', 'm', 't' ], results1);
+module.exports = map;
