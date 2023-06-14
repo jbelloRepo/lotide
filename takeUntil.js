e94fead600e464a88a861fd9fa1536d0aebad84f
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 18:41:57 -0400

    refactor function to remove dead code

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,13 @@
+/**
+ * Description: The function will return a
+ * "slice of the array with elements taken from the beginning."
+ * It should keep going until the callback/predicate returns
+ * a truthy value.
+ */
+
 // Function implementation
-const takeUntil = function (arr, callback) {
+const takeUntil = function(arr, callback) {
   let returnArr = [];
-//   console.log(arr);
   for (let i = 0; i < arr.length; i++) {
     if (callback(arr[i])) {
       break;
@@ -12,55 +18,4 @@ const takeUntil = function (arr, callback) {
   return returnArr;
 };
 
-
-// FUNCTION IMPLEMENTATION
-const successEmoji = "✅✅✅";
-const failedEmoji = "❌❌❌";
-const assertArraysEqual = function (arr1, arr2) {
-  const isEqual = eqArrays(arr1, arr2);
-
-  if (isEqual) {
-    console.log(`${successEmoji} Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`${failedEmoji} Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, (x) => x < 0);
-console.log(results1);
-
-console.log("---");
-
-const data2 = [
-  "I've",
-  "been",
-  "to",
-  "Hollywood",
-  ",",
-  "I've",
-  "been",
-  "to",
-  "Redwood",
-];
-const results2 = takeUntil(data2, (x) => x === ",");
-console.log(results2);
-
-
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;
