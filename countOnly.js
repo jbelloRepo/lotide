37bbc52248f2584cda7c48e220581351a0a66c8f
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 19:55:52 -0400

    Refactor to remove dead code and add description

--- countOnly.js
+++ countOnly.js
@@ -1,29 +1,21 @@
-// FUNCTION IMPLEMENTATION
-const successEmoji = "✅✅✅";
-const failedEmoji = "❌❌❌";
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${successEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failedEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+/**
+ * Description: This function should take in a collection of items
+ * and return counts for a specific subset of those items. It won't
+ * count everything. In order to decide what to count, it will also be
+ * given an idea of which items we care about and it will only count
+ * those, ignoring the others.
+ */
 
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+// FUNCTION IMPLEMENTATION
 const countOnly = function(allItems, itemsToCount) {
   let returnObj = {};
 
   for (const item in itemsToCount) {
     let itemCount = 0;
-    // console.log(itemsToCount[item]); //gives key's value
-
     //  if value true
     if (itemsToCount[item]) {
       //check key in allItems
       if (allItems.includes(item)) {
-        // console.log(item);
-        // count how many times its in there
         for (const anItem of allItems) {
           if (anItem === item) {
             itemCount++;
@@ -33,36 +25,7 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
-  //   console.log(returnObj);
   return returnObj;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe",
-];
-
-const result1 = countOnly(firstNames, {
-  Jason: true,
-  Karima: true,
-  Fang: true,
-  Agouhanna: false,
-});
-
-const arr = ["a", "a", "b", "c", "d", "e", "a"];
-const result2 = countOnly(arr, { a: true, d: true, b: false, f: true });
-assertEqual(result2["a"], 3);
-assertEqual(result2["b"], undefined);
-assertEqual(result2["d"], 1);
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
