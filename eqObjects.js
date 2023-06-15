200e8c2bac02462ce9d83f8d9f023e2cf88764d0
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 20:33:31 -0400

    Implement function using recursion

--- eqObjects.js
+++ eqObjects.js
@@ -1,34 +1,9 @@
 // FUNCTION IMPLEMENTATION
-const successEmoji = "✅✅✅";
-const failedEmoji = "❌❌❌";
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${successEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failedEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+const eqArrays = require("./eqArrays");
 const eqObjects = function(object1, object2) {
   let obj1Count = 0;
   let obj2Count = 0;
 
-  // check length
   for (const key1 in object1) {
     obj1Count++;
   }
@@ -40,43 +15,21 @@ const eqObjects = function(object1, object2) {
     return false;
   }
 
-  // check key and value
-  //   console.log(object1, object2);
   for (const key in object1) {
-    // console.log(object1[key], object2[key]);
-
-    //check for array
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
-    }
-
-    if (object1[key] === object2[key]) {
-      continue;
-    } else {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (typeof object1[key] === "object" && typeof object2[key] === "object") {
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
 
-  //check for arrays
-
   return true;
 };
 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
-
-const longSleeveShirtObject = {
-  size: "medium",
-  color: "red",
-  sleeveLength: "long",
-};
-console.log(eqObjects(shirtObject, longSleeveShirtObject)); // => false
-
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-console.log(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); // => true
-
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-console.log(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
+module.exports = eqObjects;
