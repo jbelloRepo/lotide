51978759d6c45b94f4c1b3e3b12bcc269b16f60e
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 20:51:27 -0400

    Implement function tests using Mocha & Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,33 @@
-const assertEqual = require('../assertEqual');
+const assert = require("chai").assert;
 const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+describe("#eqArrays", () => {
+  it("should return true for equal arrays [1, 2, 3] and [1, 2, 3]", () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it("should return false for non-equal arrays [1, 2, 3] and [1, 2, 4]", () => {
+    assert.isFalse(eqArrays([1, 2, 3], [1, 2, 4]));
+  });
+
+  it("should return true for equal arrays ['1', '2', '3'] and ['1', '2', '3']", () => {
+    assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+  });
+
+  it("should return true for equal arrays ['1', '2', '3'] and ['1', '2', 3]", () => {
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+  });
+
+  it("should return true for equal arrays [[2, 3], [4]] and [[2, 3], [4]]", () => {
+    assert.isTrue(eqArrays([[2, 3], [4]], [[2, 3], [4]]));
+  });
+
+  it("should return false for non-equal arrays [[2, 3], [4]] and [[2, 3], [4, 5]]", () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+  });
+
+  it("should return false for non-equal arrays [[2, 3], [4]] and [[2, 3], 4]", () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], 4]));
+  });
+
+});
