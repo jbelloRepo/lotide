04c68e67add862ed75a45afac42130a77df7e667
Jubril Bello <jbello@uoguelph.ca>
2023-06-14 20:45:47 -0400

    feat: Implement function using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,20 @@
-// FUNCTION IMPLEMENTATION
+// Function Implementation
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return;
+    return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
+
   return true;
 };
 
 module.exports = eqArrays;
-
-
